f14dcf42ac8ab28a96df6dad50ca094e4b4d150e
gus <grantphllps@gmail.com>
2022-04-16 02:49:47 -0500

    Added projects

---
+++ projects/alien_invasion/ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Ship:
+        """A class to manag teh ship."""
+
+        def __init__(self, ai_game):
+            """Initialize teh ship and set its starting position."""
+            self.screen = ai_game.screen
+            self.settings = ai_game.settings
+            self.screen_rect = ai_game.screen.get_rect()
+
+            #Load the ship image and get its rect.
+            self.image = pygame.image.load('images/ship.bmp')
+            self.rect = self.image.get_rect()
+
+            #Start each new ship at the bottom center of teh screen.
+            self.rect.midbottom = self.screen_rect.midbottom
+
+            #Store a decimal value for the ship's horizontal position.
+            self.x = float(self.rect.x)
+
+            # Movement flag
+            self.moving_right = 0
+            self.moving_left = 0
+
+        def update(self):
+            """Update the ship's position based on teh movement flag."""
+            if self.moving_right and self.rect.right < self.screen_rect.right:
+                self.rect.x += self.settings.ship_speed
+            elif self.moving_left and self.rect.left > 0:
+                self.rect.x -= self.settings.ship_speed
+        
+        def blitme(self):
+            """Draw teh ship at its current location."""
+            self.screen.blit(self.image,self.rect)
+
+            
